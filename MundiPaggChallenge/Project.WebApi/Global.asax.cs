e3bf18c72a8a06ee729d2952cce28e5e091f4116
Natanael Maia <gm.natanael@gmail.com>
2017-02-05 17:23:20 -0200

    WebApi
    Adding the Api documentation.

--- MundiPaggChallenge/Project.WebApi/Global.asax.cs
+++ MundiPaggChallenge/Project.WebApi/Global.asax.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+﻿using System.Web.Http;
+using System.Web.Mvc;
 
 namespace Project.WebApi
 {
@@ -12,6 +8,8 @@ namespace Project.WebApi
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            AreaRegistration.RegisterAllAreas();
         }
     }
 }
