5b99f3f5e90bc64cc552a9d65da4d6b94179fce9
Natanael Maia <gm.natanael@gmail.com>
2017-03-04 18:25:16 -0300

    WebApi Controllers
    Starting the development of api controllers for elastic search.

---
+++ MundiPaggChallenge/Project.WebApi/Controllers/ElasticSearch/PersonElasticSearchController.cs
@@ -0,0 +1,61 @@
+﻿using Project.Application.Contracts.ElasticSearch;
+using Project.WebApi.Models.ElasticSearch;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Project.WebApi.Controllers.ElasticSearch
+{
+    [RoutePrefix("api/person")]
+    public class PersonElasticSearchController : ApiController
+    {
+        private readonly IPersonElasticSearchApplicationService _elasticSerarchAppServicePerson;
+
+        public PersonElasticSearchController(IPersonElasticSearchApplicationService elasticSerarchAppServicePerson)
+        {
+            _elasticSerarchAppServicePerson = elasticSerarchAppServicePerson;
+        }
+
+        [HttpPost]
+        [Route("register")] //url: /api/person/register
+        public HttpResponseMessage Post(PersonRegisterElasticSearchModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //ToDo: make some adjusts in the automapper for item and person
+                    //ToDo: create the properties in the person model
+                    //ToDo: use the insert method (from elastic search app service) to create the person index and register the data in elastic search
+
+                    return Request.CreateResponse(HttpStatusCode.OK, "Person has been registred.");
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, GetErrorMessages());
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        private List<string> GetErrorMessages()
+        {
+            var errorsList = new List<string>();
+
+            foreach (var state in ModelState)
+            {
+                foreach (var error in state.Value.Errors)
+                {
+                    errorsList.Add(error.ErrorMessage);
+                }
+            }
+
+            return errorsList;
+        }
+    }
+}
---
+++ MundiPaggChallenge/Project.WebApi/Models/ElasticSearch/PersonElasticSearchModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.WebApi.Models.ElasticSearch
+{
+    public class PersonRegisterElasticSearchModel
+    {
+    }
+}
